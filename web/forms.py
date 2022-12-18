ef5aa4edaad169e94e19973b4e87ee5270b3131a
ttalova <talova0304@gmail.com>
2022-12-18 05:23:09 +0300

    dmin with profile and messages tables

---
+++ web/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+
+from .models import Profile
+
+
+class ProfileForm(forms.ModelForm):
+
+    class Meta:
+        model = Profile
+        fields = (
+            'external_id',
+            'name',
+        )
+        widgets = {
+            'name': forms.TextInput,
+        }
