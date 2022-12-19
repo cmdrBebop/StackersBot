e3c69d5b9458e88366e774701bb7d7433d16c62f
cmdrBebop <kostyadevyatov@gmail.com>
2022-12-19 04:07:23 +0300

    Update commands

--- bot/tgbot/handlers/commands.py
+++ bot/tgbot/handlers/commands.py
@@ -1,11 +1,9 @@
 from aiogram import Dispatcher
 from aiogram.dispatcher import FSMContext
-from aiogram.types import Message, CallbackQuery
+from aiogram.types import Message
 
 import tgbot.keyboards.inline_keyboards as inline_keyboards
 from tgbot.misc import messages
-from tgbot.misc import states
-import tgbot.misc.callbacks as callbacks
 
 
 async def command_start(message: Message, state: FSMContext):
