b658869c4ff4df316899c59c77fc00045e3232ac
darling1864 <dashaalisa2003@mail.ru>
2022-12-19 05:10:56 +0300

    1

--- stackers_bot/urls.py
+++ stackers_bot/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = 'Stackers'
+
 urlpatterns = [
     path('admin/', admin.site.urls)
 ]
